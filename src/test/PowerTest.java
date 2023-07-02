f084533a5b2479598a92631f2be645d77416ca4e
Hemanth R <hemanthrt18@gmail.com>
2023-07-02 08:50:27 +0530

    tests

---
+++ src/test/PowerTest.java
@@ -0,0 +1,26 @@
+package powerpackage;
+
+
+import org.junit.Test;
+
+public class PowerTest {
+    @Test
+    public void one_raised_to_one_is_one() {
+        assert powerfinder.of(1,1) == 1;
+    }
+
+    @Test
+    public void two_raised_to_one_is_two() {
+        assert powerfinder.of(2,1) == 2;
+    }
+
+    @Test
+    public void two_raised_to_two_is_four() {
+        assert powerfinder.of(2, 2) == 4;
+    }
+
+    @Test
+    public void three_raised_to_two_is_six() {
+        assert powerfinder.OF(3, 2) == 9;
+    }
+}
